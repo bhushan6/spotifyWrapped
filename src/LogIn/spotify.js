f28db550ad27fb5fb4fcc410d65d7d15b8ab8a30
Bhushan Wagh <bhushanwagh6@gmail.com>
2021-01-14 23:28:00 +0530

    final1.1

--- src/LogIn/spotify.js
+++ src/LogIn/spotify.js
@@ -1,8 +1,4 @@
-// Client ID 2f787d8106b84f5e98a38ad79e054168
 
-// MmY3ODdkODEwNmI4NGY1ZTk4YTM4YWQ3OWUwNTQxNjg6Y2YwOTY3MTUzMzZjNGU3NjlmYjY0MzExYzVmOGY2MjE=
-
-// curl -H "Authorization: Basic " -d grant_type=authorization_code -d code=MQCbtKe...44KN -d redirect_uri=https%3A%2F%2Fwww.foo.com%2Fauth https://accounts.spotify.com/api/token
 
 const authrise_uri = "https://accounts.spotify.com/authorize"
 const client_id = process.env.REACT_APP_CLIENT_ID
